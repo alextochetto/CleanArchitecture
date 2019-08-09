9b3697ea0c157597b5946c47522d39601420c23d
Alex Tochetto <alextochetto@gmail.com>
2019-08-08 22:25:21 -0300

    First commit

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b) => !(a == b);
+
+        public override int GetHashCode() => (GetType().GetHashCode() * 907) + Id.GetHashCode();
+
+        public override string ToString() => $"{GetType().Name} [Id={Id}]";
+    }
+}
