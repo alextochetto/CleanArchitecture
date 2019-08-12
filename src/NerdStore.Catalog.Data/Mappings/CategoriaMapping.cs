008f64c0acfad5f7bfef5a302ca8da3f0f19dfae
Alex Tochetto <alextochetto@outlook.com>
2019-08-12 13:54:13 -0300

    Some tests

---
+++ src/NerdStore.Catalog.Data/Mappings/CategoriaMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalog.Data.Mappings
+{
+    public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            // 1 : N => Categorias : Produtos
+            builder.HasMany(c => c.Produtos)
+                .WithOne(p => p.Categoria)
+                .HasForeignKey(p => p.CategoriaId);
+
+            builder.ToTable("Categorias");
+        }
+    }
+}
