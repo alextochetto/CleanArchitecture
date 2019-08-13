5f2b61f66e6325b9af45f4fd4792dd37a83038cf
Alex Tochetto <alextochetto@outlook.com>
2019-08-13 12:46:39 -0300

    connecting the dots

---
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalog.Data;
+using NerdStore.Catalog.Data.Repository;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+
+            services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+        }
+    }
+}
